e9d0130cabb871028fa4ad3659b41f6f7533669f
Henry Kim <neoroman@alterant.kr>
2014-10-02 17:15:56 +0900

    Add TODO

--- iKTV/AppDelegate.h
+++ iKTV/AppDelegate.h
@@ -5,6 +5,11 @@
 //  Created by Steve Yeom on 9/29/14.
 //  Copyright (c) 2014 2nd Jobs. All rights reserved.
 //
+/**
+ *  TODO
+ *
+ *  1) 비디오 스트리밍을 백그라운드 전환시 오디오만 나오게 할 수 있을까? Live 'DownCast'
+ */
 
 #import <UIKit/UIKit.h>
 
